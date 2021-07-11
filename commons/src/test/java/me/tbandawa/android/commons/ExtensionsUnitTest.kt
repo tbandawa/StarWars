ab61ed960a742167ab7ff052a9f36935c7ec91f3
tee <tonderaibandawa@gmail.com>
2021-07-11 06:51:21 +0200

    unit test strng extensions

--- commons/src/test/java/me/tbandawa/android/commons/ExtensionsUnitTest.kt
+++ commons/src/test/java/me/tbandawa/android/commons/ExtensionsUnitTest.kt
@@ -1,6 +1,8 @@
 package me.tbandawa.android.commons
 
 import me.tbandawa.android.commons.extensions.capitaliseFirst
+import me.tbandawa.android.commons.extensions.getResourceId
+import me.tbandawa.android.commons.extensions.getResourceType
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -12,6 +14,20 @@ class ExtensionsUnitTest {
         val str = "string"
         val capitalised = str.capitaliseFirst()
         assertEquals("S", capitalised[0].toString())
+    }
+
+    @Test
+    fun resourceId_isCorrect() {
+        val url = "https://swapi.dev/api/people/2"
+        val resourceId = url.getResourceId()
+        assertEquals(2, resourceId)
+    }
+
+    @Test
+    fun resourceType_isCorrect() {
+        val url = "https://swapi.dev/api/people/2/"
+        val resourceType = url.getResourceType()
+        assertEquals("people", resourceType)
     }
 
 }
