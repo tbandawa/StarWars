392f863d577aa55558f1e3a063dca50d13653c0e
Tendai  Bandawa <tonderaibandawa@gmail.com>
2023-06-08 22:18:46 +0200

    update search resources viewmodel and enable caching

--- data/src/commonMain/kotlin/starwars/data/viewmodel/SearchViewModel.kt
+++ data/src/commonMain/kotlin/starwars/data/viewmodel/SearchViewModel.kt
@@ -5,25 +5,56 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import starwars.data.models.BaseResource
 import starwars.data.models.RootResource
 import starwars.data.repo.StarWarsRepo
 import starwars.data.state.ResourceResult
 
 class SearchViewModel(private val starWarsRepo: StarWarsRepo): BaseViewModel() {
 
-    private val _resourceItems = MutableStateFlow<ResourceResult<Any>>(ResourceResult.Empty)
-    val resourceItems: StateFlow<ResourceResult<Any>> = _resourceItems
+    private var pageNumber = 0
+    private var resourceName = String()
+    private var resultsList = mutableListOf<Any>()
+
+    private val _resourceResults = MutableStateFlow<ResourceResult<Any>>(ResourceResult.Empty)
+    val resourceResults: StateFlow<ResourceResult<Any>> = _resourceResults
+
+    private val _resourceItems = MutableStateFlow(emptyList<Any>())
+    val resourceItems: StateFlow<List<Any>> = _resourceItems
 
     fun searchResources(resourceType: String, search: String, page: Int) {
         coroutineScope.launch {
             starWarsRepo.searchResources(resourceType, search, page).collect { results ->
-                _resourceItems.value = results
+                if (results is ResourceResult.Success) {
+
+                    // If page <= pageNumber and resource type is not
+                    // equal to cached type, clear cached resultsList
+                    if ((page <= pageNumber) && (resourceType != resourceName)) {
+                        resultsList = mutableListOf()
+                    }
+
+                    // Otherwise append results to cached resultsList
+                    ((results.data as BaseResource<*>).results as MutableList<*>).forEach {
+                        resultsList += it as Any
+                    }
+                    _resourceItems.value = resultsList.toList()
+                    resourceName = resourceType
+                    pageNumber = page
+
+                }
+                _resourceResults.value = results
             }
         }
     }
 
+    fun observeResourceResults(provideNewState: ((ResourceResult<Any>) -> Unit)) {
+        _resourceResults.onEach {
+            provideNewState.invoke(it)
+        }.launchIn(coroutineScope)
+    }
+
     @Suppress("unused")
-    fun observeResourceItems(provideNewState: ((ResourceResult<Any>) -> Unit)) {
+    fun observeResourceItems(provideNewState: ((List<Any>) -> Unit)) {
         _resourceItems.onEach {
             provideNewState.invoke(it)
         }.launchIn(coroutineScope)
