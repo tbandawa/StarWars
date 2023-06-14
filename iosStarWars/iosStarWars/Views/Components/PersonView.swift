2faf7f24445fa8ce7fab0a42207803438a97e7bc
Tendai  Bandawa <tonderaibandawa@gmail.com>
2023-06-14 08:58:48 +0200

    create person view

--- iosStarWars/iosStarWars/Views/Components/PersonView.swift
+++ iosStarWars/iosStarWars/Views/Components/PersonView.swift
@@ -14,108 +14,226 @@ struct PersonView: View {
     let person: Person
     
     var body: some View {
-        List {
+        
+        ScrollView {
             
-            Section {
-                Text(person.height)
-            } header: {
-                Text("Height")
-            }
-            .listRowSeparator(.hidden)
-            
-            Section {
-                Text(person.mass)
-            } header: {
-                Text("Mass")
-            }
-            .listRowSeparator(.hidden)
-            
-            Section {
-                Text(person.hair_color)
-            } header: {
-                Text("Hair Color")
-            }
-            .listRowSeparator(.hidden)
-            
-            Section {
-                Text(person.skin_color)
-            } header: {
-                Text("Skin Color")
-            }
-            .listRowSeparator(.hidden)
-            
-            Section {
-                Text(person.eye_color)
-            } header: {
-                Text("Eye Color")
-            }
-            .listRowSeparator(.hidden)
-            
-            Section {
-                Text(person.birth_year)
-            } header: {
-                Text("DOB")
-            }
-            .listRowSeparator(.hidden)
-            
-            Section {
-                Text(person.gender)
-            } header: {
-                Text("Gender")
-            }
-            .listRowSeparator(.hidden)
-            
-            Section {
-                Text(person.homeworld)
-            } header: {
-                Text("Home World")
-            }
-            .listRowSeparator(.hidden)
-            
-            /*Section {
-                ForEach(person.films, id: \.self) { film in
-                    Text("\(film)")
+            LazyVStack(alignment: .leading) {
+                
+                HStack {
+                    
+                    VStack {
+                        Text("Height")
+                            .font(.system(size: 18))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                            .padding(5)
+                        Spacer()
+                        Text(person.height)
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+                            .padding(5)
+                    }
+                    .frame(minWidth: 40, maxHeight: 100)
+                    .background(Color.black)
+                    .cornerRadius(8)
+                    
+                    VStack {
+                        Text("Mass")
+                            .font(.system(size: 18))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                            .padding(5)
+                        Spacer()
+                        Text(person.mass)
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+                            .padding(5)
+                    }
+                    .frame(minWidth: 40, maxHeight: 100)
+                    .background(Color.black)
+                    .cornerRadius(8)
+                    
+                    VStack {
+                        Text("Skin Color")
+                            .font(.system(size: 18))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                            .padding(5)
+                        Spacer()
+                        Text(person.skin_color)
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+                            .padding(5)
+                    }
+                    .frame(minWidth: 40, maxHeight: 100)
+                    .background(Color.black)
+                    .cornerRadius(8)
+                                    
                 }
-            } header: {
-                Text("Films")
-            }
-            .listRowSeparator(.hidden)
-            
-            Section {
-                ForEach(person.species, id: \.self) { species in
-                    Text("\(species)")
+                
+                HStack {
+                    
+                    VStack {
+                        Text("Height")
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                            .padding(5)
+                        Spacer()
+                        Text(person.height)
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+                            .padding(5)
+                    }
+                    .frame(minWidth: 40, maxHeight: 100)
+                    .background(Color.black)
+                    .cornerRadius(8)
+                    
+                    VStack {
+                        Text("Mass")
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                            .padding(5)
+                        Spacer()
+                        Text(person.mass)
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+                            .padding(5)
+                    }
+                    .frame(minWidth: 40, maxHeight: 100)
+                    .background(Color.black)
+                    .cornerRadius(8)
+                    
+                    VStack {
+                        Text("Skin Color")
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                            .padding(5)
+                        Spacer()
+                        Text(person.skin_color)
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+                            .padding(5)
+                    }
+                    .frame(minWidth: 40, maxHeight: 100)
+                    .background(Color.black)
+                    .cornerRadius(8)
+                                    
                 }
-            } header: {
-                Text("Species")
-            }
-            .listRowSeparator(.hidden)
-            
-            Section {
-                ForEach(person.vehicles, id: \.self) { vehicle in
-                    Text("\(vehicle)")
+                
+                HStack {
+                    
+                    VStack {
+                        Text("Home World")
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                            .padding(5)
+                        Spacer()
+                        Text(person.homeworld)
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+                            .padding(5)
+                    }
+                    .frame(minWidth: 40, maxHeight: 100)
+                    .background(Color.black)
+                    .cornerRadius(8)
+                                    
                 }
-            } header: {
-                Text("Vehicles")
-            }
-            .listRowSeparator(.hidden)
-            
-            Section {
-                ForEach(person.starships, id: \.self) { starship in
-                    Text("\(starship)")
+                .padding(.bottom, 15)
+                
+                if person.films.count > 0 {
+                    LazyVStack(alignment: .leading) {
+                        Text("Films")
+                            .font(.system(size: 20, weight: .heavy, design: .default))
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                        ForEach(person.films, id: \.self) { film in
+                            Text("\(film)")
+                                .padding(5)
+                                .font(.system(size: 14, weight: .regular, design: .rounded))
+                                .foregroundColor(.white)
+                                .background(.black)
+                                .cornerRadius(30)
+                        }
+                    }
+                    .padding(.bottom, 15)
                 }
-            } header: {
-                Text("Starships")
+                
+                if person.species.count > 0 {
+                    LazyVStack(alignment: .leading) {
+                        Text("Species")
+                            .font(.system(size: 20, weight: .heavy, design: .default))
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                        ForEach(person.species, id: \.self) { species in
+                            Text("\(species)")
+                                .padding(5)
+                                .font(.system(size: 14, weight: .regular, design: .rounded))
+                                .foregroundColor(.white)
+                                .background(.black)
+                                .cornerRadius(30)
+                        }
+                    }
+                    .padding(.bottom, 15)
+                }
+                
+                if person.vehicles.count > 0 {
+                    LazyVStack(alignment: .leading) {
+                        Text("Vehicles")
+                            .font(.system(size: 20, weight: .heavy, design: .default))
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                        ForEach(person.vehicles, id: \.self) { vehicle in
+                            Text("\(vehicle)")
+                                .padding(5)
+                                .font(.system(size: 14, weight: .regular, design: .rounded))
+                                .foregroundColor(.white)
+                                .background(.black)
+                                .cornerRadius(30)
+                        }
+                    }
+                    .padding(.bottom, 15)
+                }
+                
+                if person.starships.count > 0 {
+                    LazyVStack(alignment: .leading) {
+                        Text("Starships")
+                            .font(.system(size: 20, weight: .heavy, design: .default))
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                        ForEach(person.starships, id: \.self) { starship in
+                            Text("\(starship)")
+                                .padding(5)
+                                .font(.system(size: 14, weight: .regular, design: .rounded))
+                                .foregroundColor(.white)
+                                .background(.black)
+                                .cornerRadius(30)
+                        }
+                    }
+                    .padding(.bottom, 15)
+                }
+                
+                Text(person.created)
+                    .font(.system(size: 12))
+                    .foregroundColor(.black)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(5)
+                
             }
-            .listRowSeparator(.hidden)*/
+            .padding(.leading, 16)
+            .padding(.trailing, 16)
             
         }
-        .listStyle(.inset)
+        
     }
 }
-
-/*struct PersonView_Previews: PreviewProvider {
-    
-    static var previews: some View {
-        PersonView()
-    }
-}*/
