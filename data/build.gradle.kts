ba774cf87b24f6765ba48a1f805517c277a86767
Tendai  Bandawa <tonderaibandawa@gmail.com>
2024-12-10 04:42:02 +0200

    copy toml file

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -79,4 +79,8 @@ android {
         minSdk = 24
         targetSdk = 34
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
